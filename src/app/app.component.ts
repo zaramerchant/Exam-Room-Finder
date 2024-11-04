4ed87cdcf8cb0a095f54f9ac4ef1edafe3bf026c
Zara Merchant <zaramerchant33@gmail.com>
2024-11-04 00:09:17 -0500

    Adding analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http'; 
+import { inject } from '@vercel/analytics';
 
 
 @Component({
@@ -12,7 +13,10 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'exam_loc_finder';
 
+  ngOnInit(): void {
+    inject();
+  }
 }
